a414f839bd08f6a3678a936d0b823ae99d781ffa
consistencycoder <emailacctspare1@gmail.com>
2024-08-22 19:21:48 -0400

    Changed the string in simple_message.py

--- ch2_variables_simple_data_types/simple_message.py
+++ ch2_variables_simple_data_types/simple_message.py
@@ -5,5 +5,5 @@
 # explanation: A variable named message is assigned the string value
 # then the variable is printed, so its contents will be the output
 
-message = "This is the message I am assigning to the variable named message"
+message = "This is the string I am assigning to the variable named message"
 print(message)
