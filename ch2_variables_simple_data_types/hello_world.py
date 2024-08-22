ef88605f253cee3677438e3c424cd94bc177d421
consistencycoder <emailacctspare1@gmail.com>
2024-08-21 22:28:04 -0400

    Examples from ch.2

--- ch2_variables_simple_data_types/hello_world.py
+++ ch2_variables_simple_data_types/hello_world.py
@@ -1,2 +1,17 @@
+# p.15 example 1: the interpreter
+# example 1 output: Hello Python world!
+# example 1 explanation:
+# this example was used to talk about the Python interpreter
+# when you run a Python file, the interpreter reads the code line-by-line 
+# and it translates the code into instructions that a computer can 
+# comprehend and run
+print("Hello Python world!")
+
+# p.16 example 2: introducing a variable
+# example 2 output: Hello Python world!
+# example 2 explanation: 
+# you assigned a string value to a variable named message
+# when you wrote the print message, you said to print
+# the value contained in the variable named message
 message = "Hello Python world!"
 print(message)
