f50de290396ba269b7c21598e607011067a90991
consistencycoder <emailacctspare1@gmail.com>
2024-08-21 22:39:15 -0400

    Ch.1 example comments

--- ch1_getting_started/hello_world.py
+++ ch1_getting_started/hello_world.py
@@ -1,1 +1,9 @@
-print("Hello Python interpreter!")
+# p.10 example: Running hello_world.py
+# example output: Hello Python world!
+# example explanation:
+# the objective of this example was to run this file via the terminal.
+# the overarching idea of this whole chapter was to get acquainted with
+# Python via running some code snippets through a terminal session
+# and also running a Python program named hello_world.py 
+# through the terminal.
+print("Hello Python world!")
