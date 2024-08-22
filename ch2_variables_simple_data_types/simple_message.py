32a1e87bcb49dc6af7761fcde233cfae0ef9debe
consistencycoder <emailacctspare1@gmail.com>
2024-08-22 18:58:53 -0400

    simple_message.py was created for practice

---
+++ ch2_variables_simple_data_types/simple_message.py
@@ -0,0 +1,9 @@
+# Try it yourself: 
+# 2-1. Simple Message: Assign a message to a variable, 
+# and then print that message
+# output: This is the message I am assigning to the variable named message
+# explanation: A variable named message is assigned the string value
+# then the variable is printed, so its contents will be the output
+
+message = "This is the message I am assigning to the variable named message"
+print(message)
