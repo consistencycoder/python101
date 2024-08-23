280aba7aa5c099829424f3ce45f433db8da5782e
consistencycoder <emailacctspare1@gmail.com>
2024-08-23 14:25:34 -0400

    Created name.py

---
+++ ch2_variables_simple_data_types/name.py
@@ -0,0 +1,11 @@
+# p.20 example: Changing Case in a String with Methods
+# example output: Ada Lovelace
+# example explanation:
+# this example shows how to use the .title() method on a string.
+# the variable named name, is assigned the lowercase string "ada lovelace."
+# the .title() method converts the first letter of each word to uppercase.
+# the print statement outputs the string with each word capitalized to 
+# Ada Lovelace.
+
+name = "ada lovelace"
+print(name.title())
