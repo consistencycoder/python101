6acba4f30ca10d5e18ac01c398bf001d45544ac8
consistencycoder <emailacctspare1@gmail.com>
2024-08-23 14:15:20 -0400

    simple_messages.py was created for try it yourself 2-2

---
+++ ch2_variables_simple_data_types/simple_messages.py
@@ -0,0 +1,18 @@
+# Try it yourself: 
+# 2-2. Simple Messages: Assign a message to a variable, 
+# and print that message. Then change the value of the variable 
+# to a new message, and print the new message
+# output: 
+# This is the message I am assigning to the variable named message
+# This is the NEW string I am assigning to message
+# explanation: A variable named message is assigned an initial 
+# string value, and that value is printed. Then, the variable is
+# reassigned another string value, and that value is printed.
+# This is an example of variable reassignment.
+
+message = "This is the string I am assigning to the variable named message"
+print(message)
+
+# now I'm going to reassign the variable message then print it
+message = "This is the NEW string I am assigning to message"
+print(message)
