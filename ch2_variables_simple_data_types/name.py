966d556691e4bfca1ae5b1b67dc3bda69e4481de
consistencycoder <emailacctspare1@gmail.com>
2024-08-23 14:54:09 -0400

    name.py examples added

--- ch2_variables_simple_data_types/name.py
+++ ch2_variables_simple_data_types/name.py
@@ -1,4 +1,4 @@
-# p.20 example: Changing Case in a String with Methods
+# p.20 example 1: Changing Case in a String with Methods
 # example output: Ada Lovelace
 # example explanation:
 # this example shows how to use the .title() method on a string.
@@ -6,6 +6,22 @@
 # the .title() method converts the first letter of each word to uppercase.
 # the print statement outputs the string with each word capitalized to 
 # Ada Lovelace.
+# the examples after this follow the same idea
 
+print("changing case with .title() method p.20 ex.1 :")
 name = "ada lovelace"
 print(name.title())
+
+# p.20 example 2: using the method .upper() to change
+# a string to all uppercase letters
+
+print("changing to all uppercase with .upper() p.20 ex.2:")
+name = "Ada Lovelace"
+print(name.upper())
+
+# p.20 example 3: using the method .lower() to change
+# a string to all lowercase letters
+
+print("changing to all lowercase with .lower() p.20 ex.3:")
+name = "Ada Lovelace"
+print(name.lower())
