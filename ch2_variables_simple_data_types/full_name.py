bf5d82017a096fec386c807f9fe7df6b2edfcb04
consistencycoder <emailacctspare1@gmail.com>
2024-08-24 22:26:04 -0400

    Added full_name.py

---
+++ ch2_variables_simple_data_types/full_name.py
@@ -0,0 +1,11 @@
+# p.21 example 1: using variables in strings
+# example 1 output: ada lovelace
+# this is the first example of a formatted string, or f-string.
+# f-strings let you put the value of Python expressions 
+# into a string by using the syntax f"{var_name}".
+# f-strings allow you to make dynamic strings.
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
