5c6d1d172fca43f9ae6ff8f0f26459686c7e79c7
consistencycoder <emailacctspare1@gmail.com>
2024-08-25 22:03:19 -0400

    Added another example to whitespace.py

--- ch2_variables_simple_data_types/ch2_examples/whitespace.py
+++ ch2_variables_simple_data_types/ch2_examples/whitespace.py
@@ -1,7 +1,17 @@
-# p.22 example 1: Adding Whitespace to Strings with Tabs or New Lines
-# example output:     Python (tab to the left)
+# p.22 example 1: Adding Whitespace to Strings with Tabs or Newlines
+# example output:     
+#   Python (tab to the left)
 # example explanation:
 # This example shows that you can add whitespace in the form of a 
 # tab by using the escape symbol \t.
 
 print("\tPython")
+
+# p.22 example 2: Using a newline
+# example output:
+# Languages:
+# Python
+# C
+# JavaScript
+
+print("Languages:\nPython\nC\nJavaScript") 
